e756547ee270a98558adc1c7e27380c79f54e37c
artchar <arthr2@ucla.edu>
2015-04-07 02:10:10 -0700

    more stylez

--- client/categories.js
+++ client/categories.js
@@ -1,5 +1,19 @@
 Template.categories.helpers({
-	category1: function(){
-		return "Hi";
+	swag: function(){
+		if (Session.get("hi"))
+			return "selected";
+		else
+			return "";
+	}
+});
+
+Template.categories.events({
+	"click #h": function(event, template) {
+		if (Session.get("hi"))
+			Session.set("hi", false);
+		else
+			Session.set("hi", true);
+
+		console.log("hei");
 	}
 });

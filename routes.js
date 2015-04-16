6f04c6a4dbb7befddf9801ac1799c8aa328c50c0
artchar <arthr2@ucla.edu>
2015-04-15 21:44:08 -0700

    first collection

--- routes.js
+++ routes.js
@@ -1,3 +1,13 @@
 Router.map(function() {
 	this.route('game', {path: '/'});
 });
+
+if (Meteor.isClient) {
+	Template.cluescreen.events({
+		"click button": function (event, template) {
+			event.preventDefault();
+			var t = template.find("#answer").value;
+			alert(t);
+		}
+	});
+}

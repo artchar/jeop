1fb01bc56f8c51584b0526a526a28899ca86690f
artchar <arthr2@ucla.edu>
2015-04-16 03:10:48 -0700

    collection test

--- collections.js
+++ collections.js
@@ -1,1 +1,54 @@
 Clues = new Mongo.Collection("clues");
+
+if (Meteor.server) {
+	Clues.insert({
+		category: "GAMES",
+		clues: [
+			"the capital of dick mofuckaaaa",
+			"shit boss bro",
+			"stop",
+			"sexxx",
+			"hey"
+		]
+	});
+	Clues.insert({
+		category: "g",
+		clues: [
+			"the capital of dick mofuckaaaa",
+			"shit boss bro",
+			"stop",
+			"sexxx",
+			"hey"
+		]
+	}); 
+	Clues.insert({
+		category: "b",
+		clues: [
+			"the capital of dick mofuckaaaa",
+			"shit boss bro",
+			"stop",
+			"sexxx",
+			"hey"
+		]
+	}); 
+	Clues.insert({
+		category: "sdf",
+		clues: [
+			"the capital of dick mofuckaaaa",
+			"shit boss bro",
+			"stop",
+			"sexxx",
+			"hey"
+		]
+	}); 
+	Clues.insert({
+		category: "fee",
+		clues: [
+			"the capital of dick mofuckaaaa",
+			"shit boss bro",
+			"stop",
+			"sexxx",
+			"hey"
+		]
+	});
+}

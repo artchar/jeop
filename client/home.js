9360a480f28f3e24f21a8d000f8fd7b48a2160ec
artchar <arthr2@ucla.edu>
2015-04-17 04:47:53 -0700

    trying to fix userid stuff

--- client/home.js
+++ client/home.js
@@ -2,7 +2,9 @@ Template.home.events({
 	"submit form": function(event) {
 		event.preventDefault();
 		var nick = event.target.nickname.value;
-		console.log(nick);
 		Meteor.call("addUser", nick);
+	},
+
+	"click button": function(event) {
 	}
-})
+});
--- methods.js
+++ methods.js
@@ -1,10 +1,8 @@
 Meteor.methods({
 	addUser: function(nickname) {
 		check(nickname, String);
-		var userid = Users.insert({
-			nickname: nickname
-		});
-		this.setUserId(userid);
-		console.log(this.userId);
+	},
+
+	test:function() {
 	}
 });

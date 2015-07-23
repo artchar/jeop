45c7aa0c708e58d71144e09578c6f5ab346a5c3f
artchar <artchar@gmail.com>
2015-07-23 03:44:09 -0700

    start scraper

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -7,3 +7,8 @@ import urllib2
 # Download j-archive html files
 # Use Beautiful Soup to parse files
 # Store categories, questions, and answers in JSON documents, save to files
+
+html = urllib2.urlopen("http://www.j-archive.com/showgame.php?game_id=1144")
+
+soup = BeautifulSoup(html, 'html.parser')
+print(soup.title.contents[0])

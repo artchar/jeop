105bb4ef07d06c2a7cd0359f0dc6fcaa832453d7
artchar <artchar@gmail.com>
2015-07-24 04:01:40 -0700

    scraper

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -8,7 +8,19 @@ import urllib2
 # Use Beautiful Soup to parse files
 # Store categories, questions, and answers in JSON documents, save to files
 
-html = urllib2.urlopen("http://www.j-archive.com/showgame.php?game_id=1144")
+for i in range(2542, 2543):
+	url = "http://www.j-archive.com/showgame.php?game_id=" + str(i)
+	html = urllib2.urlopen(url)
 
-soup = BeautifulSoup(html, 'html.parser')
-print(soup.title.contents[0])
+	soup = BeautifulSoup(html, 'html.parser')
+
+	# Get categories that don't have 5 clues
+
+	for category in range(1, 6):
+		for clue in range(1, 5):
+			if soup.find(id="clue_J_" + str(category) + "_" + str(clue)) == None:
+				print i
+				print category
+				print clue
+	#if "a href" in str(soup.find(id="clue_J_1_1")):
+	print type(str(soup.find(id="clue_J_1_1")))

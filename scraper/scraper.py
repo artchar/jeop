c2b16d9ad3e6153d4413786e074e4e79be8dcf49
artchar <artchar@gmail.com>
2015-07-26 04:29:55 -0700

    scraper

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -8,20 +8,40 @@ import urllib2
 # Use Beautiful Soup to parse files
 # Store categories, questions, and answers in JSON documents, save to files
 
-for i in range(2542, 2543):
+def clueIterate(list, cat, soup):
+	for clue in range(1, 6):
+			clueid = "clue_J_" + str(cat) + "_" + str(clue)
+			if soup.find(id=clueid) == None or "a href" in str(soup.find(clueid)):
+				dontuse.append(cat)
+				return
+
+for i in range(2542, 2600):
 	url = "http://www.j-archive.com/showgame.php?game_id=" + str(i)
 	html = urllib2.urlopen(url)
 
 	soup = BeautifulSoup(html, 'html.parser')
 
 	# Get categories that don't have 5 clues
+	dontuse = []
 
-	for category in range(1, 6):
-		for clue in range(1, 5):
-			clueid = "clue_J_" + str(category) + "_" + str(clue))
-			if soup.find(id=clueid) == None or "a href" in str(soup.find(clueid):
-				print i
-				print category
-				print clued
+	for category in range(1, 7):
+		clueIterate(dontuse, category, soup)
+	
+	for category in range(1, 7):
+		if category in dontuse:
+			continue
+
+		cat = soup.findAll(class_="category_name")[category].contents[0]
+		catComments = soup.findAll(class_="category_comments")[category].contents[0]	
+		
+		for clue in range(1, 6):
+			clueid = "clue_J_" + str(category) + "_" + str(clue)
+		print cat
+
+
+
 	#if "a href" in str(soup.find(id="clue_J_1_1")):
-	print type(str(soup.find(id="clue_J_1_1")))
+	#print type(str(soup.find(id="clue_J_1_1")))
+	# for x in dontuse:
+	# 	print url
+	# 	print x

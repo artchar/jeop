e9279e69f4085963705cad58156e95bc9dd34f1d
artchar <artchar@gmail.com>
2015-07-27 20:26:00 -0700

    remove limiting, til meteor releases

--- server/limiting.js
+++ server/limiting.js
@@ -9,12 +9,12 @@
 // // Add the rule, allowing up to 1 call every 2 seconds
 // DDPRateLimiter.addRule(checkAnswerLimit, 1, 2000);
 
-EasySecurity.config({
-  general: { type: 'rateLimit', ms: 1500 },
-  methods: {
-    joinRoom: { type: 'throttle', ms: 1000 },
-    clickClue: { type: 'throttle', ms: 300 }
-  },
-  ignoredMethods: ['playerCleanup', 'roomCleanup', 'ping', 'playerLeave', 'login'],
-  maxQueueLength: 200
-});
+// EasySecurity.config({
+//   general: { type: 'rateLimit', ms: 1500 },
+//   methods: {
+//     joinRoom: { type: 'rateLimit', ms: 1000 },
+//     clickClue: { type: 'rateLimit', ms: 300 }
+//   },
+//   ignoredMethods: ['playerCleanup', 'roomCleanup', 'ping', 'playerLeave', 'login'],
+//   maxQueueLength: 200
+// });

12d95ec367394add4abb82685f8ea7d647b4e548
artchar <artchar@gmail.com>
2015-07-28 00:54:50 -0700

    editor progress

--- scraper/editor.py
+++ scraper/editor.py
@@ -5,9 +5,16 @@ import json
 suffix = ".json"
 
 fileNumber = raw_input("File number? ")
-fd = open(fileNumber + suffix, "rw")
+fd = open("clues/" + fileNumber + suffix, "rw")
 lines = fd.readlines()
 newdata= []
 for x in lines:
 	 newdata.append(json.loads(x))
-print len(newdata)
+
+for d in newdata:
+	for i in range(0, 5):
+		oldAnswers = d["clues"][i]["answer"]
+		print oldAnswers
+
+		s = raw_input("q: " + d["clues"][i]["question"])
+		print s

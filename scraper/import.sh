52086e90b12e36f64e1e1839a37dec78dc1d4172
artchar <artchar@gmail.com>
2015-07-31 00:16:22 -0700

    stuff works now!

--- scraper/import.sh
+++ scraper/import.sh
@@ -1,4 +1,4 @@
 for f in done_clues/*.json
 do
-	mongoimport --host production-db-d2.meteor.io --port 27017 --username client-195be45d --password 1247bbb7-c5d7-23d4-d234-ccf022cd1475 --db jepp_meteor_com --collection clues --file $f
+	mongoimport --host production-db-d2.meteor.io --port 27017 --username client-54b0388e --password e4dfd533-9702-e09a-cf65-40c5f81ff4fc --db jepp_meteor_com --collection clues --file $f
 done

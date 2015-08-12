af091b1c4f68a8798b2c52348ffb671aba844b9c
artchar <arthr2@ucla.edu>
2015-08-12 09:08:20 -0700

    Update import.sh
    whoops forgot about sensitive data

--- scraper/import.sh
+++ scraper/import.sh
@@ -1,4 +1,4 @@
 for f in done_clues/*.json
 do
-	mongoimport --host apollo.modulusmongo.net --port 27017 --username tawwdmuncru --password ZsnakR97v --db en4ipuRi --collection clues --file $f
+	mongoimport --host apollo.modulusmongo.net --port 27017 --username user --password pass --db en4ipuRi --collection clues --file $f
 done

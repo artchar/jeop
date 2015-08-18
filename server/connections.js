db0e909ecbddfcbacaf085c23df920b7a69bf2b9
artchar <arthr2@ucla.edu>
2015-08-18 05:13:38 -0700

    Update connections.js

--- server/connections.js
+++ server/connections.js
@@ -1,6 +1,7 @@
+// Client calls this to let server know they're still connected
+
 Meteor.methods({
 	ping: function() {
-
 		this.unblock();
 		var now = new Date().getTime();
 		Meteor.users.update({_id: Meteor.userId()},
